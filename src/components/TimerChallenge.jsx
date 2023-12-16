39075ce27518400f2e12cb0fb9fe1b7e96959988
Danbi Choi <sweetrain05@hotmail.com>
2023-12-15 19:55:49 -0800

    useImperativeHandle hook : exposing component API

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,7 +10,7 @@ const TimerChallenge = ({ title, targetTime }) => {
     const handleStart = () => {
         timer.current = setTimeout(() => {
             setTimerExpired(true);
-            dialog.current.showModal();
+            dialog.current.open();
         }, targetTime * 1000);
 
         setTimerStarted(true);
